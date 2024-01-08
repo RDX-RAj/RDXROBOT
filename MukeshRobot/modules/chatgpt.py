dc435e2f8d8fea485f2fab7a0eb20408a1a1f7ba
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-08 14:13:28 +0530

    Update chatgpt.py

--- MukeshRobot/modules/chatgpt.py
+++ MukeshRobot/modules/chatgpt.py
@@ -3,7 +3,7 @@ from .. import pbot as Mukesh,BOT_NAME,BOT_USERNAME
 import time
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
-@Mukesh.on_message(filters.command(["chatgpt","ai","ask"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
+@Mukesh.on_message(filters.command(["gpt","ai"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
 async def chat(bot, message):
     
     try:
