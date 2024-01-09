309e3b4de6d21398a1d577a6fec7e48187feec59
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-09 09:06:51 +0530

    Update source.py

--- MukeshRobot/modules/source.py
+++ MukeshRobot/modules/source.py
@@ -16,17 +16,15 @@ Mukesh = "https://telegra.ph/file/6f04cb09ff8e8af19ae02.jpg"
 async def repo(client, message):
     await message.reply_photo(
         photo=Mukesh,
-        caption=f"""**๏ ʜᴇʏ {message.from_user.mention()},\n\n๏ ɪ ᴀᴍ [{dispatcher.bot.first_name}](t.me/{dispatcher.bot.username})**\n\n๏ **ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴀᴠɪsʜᴀ ʙᴏᴛ ʀᴇᴘᴏ, ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʀᴇᴘᴏ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ᴍʏ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ.**
+        caption=f"""**๏ ʜᴇʏ {message.from_user.mention()}, ᴡᴇʟᴄᴏᴍᴇ ʙᴀʙʏ !\n\n๏ ɪ ᴀᴍ [{dispatcher.bot.first_name}](t.me/{dispatcher.bot.username})**\n\n๏ **ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴀᴠɪsʜᴀ ʙᴏᴛ ʀᴇᴘᴏ, ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʀᴇᴘᴏ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ᴍʏ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ.**
 """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "ᴏᴡɴᴇʀ",user_id=OWNER_ID
+                        "sᴜᴘᴘᴏʀᴛ", url="https://the_friendz",
                     ),
-                    InlineKeyboardButton(
-                        "ʀᴇᴘᴏ",
-                        url="https://t.me/roy_editx",
+                    InlineKeyboardButton(text="ʀᴇᴘᴏ", callback_data="source_",
                     ),
                 ]
             ]
