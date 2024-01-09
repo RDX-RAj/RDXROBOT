c1a1e53dc02508316603f36acdcc4078fa7a0ce5
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-09 05:53:23 +0530

    Create source.py

---
+++ MukeshRobot/modules/source.py
@@ -0,0 +1,49 @@
+from platform import python_version as y
+
+from pyrogram import __version__ as z
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import __version__ as o
+from telethon import __version__ as s
+
+from MukeshRobot import OWNER_ID, dispatcher
+from MukeshRobot import pbot as client
+
+Mukesh = "https://te.legra.ph/file/1a72f3770dcb90ee8b3f7.jpg"
+
+
+@client.on_message(filters.command(["repo", "source"]))
+async def repo(client, message):
+    await message.reply_photo(
+        photo=Mukesh,
+        caption=f"""**ʜᴇʏ {message.from_user.mention()},\n\nɪ ᴀᴍ [{dispatcher.bot.first_name}](t.me/{dispatcher.bot.username})**
+
+**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ :** [ʀᴏʏ-ᴇᴅɪᴛx](tg://user?id={OWNER_ID})
+**» ᴩʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{y()}`
+**» ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{o}` 
+**» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{s}` 
+**» ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{z}`
+
+**ᴀᴠɪsʜᴀ sᴏᴜʀᴄᴇ ɪs ɴᴏᴡ ᴩᴜʙʟɪᴄ ᴀɴᴅ ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ.**
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "ᴏᴡɴᴇʀ",user_id=OWNER_ID
+                    ),
+                    InlineKeyboardButton(
+                        "ʀᴇᴘᴏ",
+                        url="https://t.me/roy_editx",
+                    ),
+                ]
+            ]
+        ),
+    )
+
+
+__mod_name__ = "Rᴇᴩᴏ"
+_help__ = """
+ /repo  ᴛᴏ ɢᴇᴛ ʀᴇᴘᴏ 
+ /source ᴛᴏ ɢᴇᴛ ʀᴇᴘᴏ
+"""
