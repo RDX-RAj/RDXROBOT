b37ba74e0e72627dd4f842958a9c750921e2c9c9
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-09 09:08:08 +0530

    Update source.py

--- MukeshRobot/modules/source.py
+++ MukeshRobot/modules/source.py
@@ -16,7 +16,7 @@ Mukesh = "https://telegra.ph/file/6f04cb09ff8e8af19ae02.jpg"
 async def repo(client, message):
     await message.reply_photo(
         photo=Mukesh,
-        caption=f"""**๏ ʜᴇʏ {message.from_user.mention()}, ᴡᴇʟᴄᴏᴍᴇ ʙᴀʙʏ !\n\n๏ ɪ ᴀᴍ [{dispatcher.bot.first_name}](t.me/{dispatcher.bot.username})**\n\n๏ **ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴀᴠɪsʜᴀ ʙᴏᴛ ʀᴇᴘᴏ, ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʀᴇᴘᴏ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ᴍʏ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ.**
+        caption=f"""**๏ ʜᴇʏ {message.from_user.mention()}, ᴡᴇʟᴄᴏᴍᴇ ʙᴀʙʏ !\n\n๏ ɪ ᴀᴍ [{dispatcher.bot.first_name}](t.me/{dispatcher.bot.username})**\n\n๏ **ɪғ ʏᴏᴜ ᴡᴀɴᴛ ๛ᴀ ᴠ ɪ s ʜ ᴀ ༗ ʙᴏᴛ ʀᴇᴘᴏ, ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʀᴇᴘᴏ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ᴍʏ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ.**
 """,
         reply_markup=InlineKeyboardMarkup(
             [
