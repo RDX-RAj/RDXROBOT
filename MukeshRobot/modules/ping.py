6344500ca26c030f03d95403033771249505de3b
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-11 08:24:47 +0530

    Update ping.py

--- MukeshRobot/modules/ping.py
+++ MukeshRobot/modules/ping.py
@@ -73,15 +73,15 @@ def ping(update: Update, context: CallbackContext):
     msg = update.effective_message
 
     start_time = time.time()
-    message = msg.reply_text("🏓 ᴘɪɴɢɪɴɢ ʙᴀʙʏ....​")
+    message = msg.reply_text("❍ ᴘɪɴɢɪɴɢ ʙᴀʙʏ....​")
     end_time = time.time()
     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ! ❤️\n"
-        "<b>ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:</b> <code>{}</code>\n"
-        "<b>ᴜᴘᴛɪᴍᴇ:</b> <code>{}</code>".format(telegram_ping, uptime),
+        "❍ ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ 💥\n"
+        "❍ <b>ᴛɪᴍᴇ ᴛᴀᴋᴇɴ ➛</b> <code>{}</code>\n"
+        "❍ <b>ᴜᴘᴛɪᴍᴇ ➛</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
     )
 
@@ -93,7 +93,7 @@ def pingall(update: Update, context: CallbackContext):
     pinged_list.insert(2, "")
     uptime = get_readable_time((time.time() - StartTime))
 
-    reply_msg = "⏱ᴘɪɴɢ ʀᴇsᴜʟᴛs ᴀʀᴇ:\n"
+    reply_msg = "❍ ᴘɪɴɢ ʀᴇsᴜʟᴛs ᴀʀᴇ:\n"
     reply_msg += "\n".join(pinged_list)
     reply_msg += "\n<b>ᴜᴘᴛɪᴍᴇ:</b> <code>{}</code>".format(uptime)
 
