127badd01e878b181aef17ade5c655241e13e065
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-12 15:10:17 +0530

    Update country.py

--- MukeshRobot/modules/country.py
+++ MukeshRobot/modules/country.py
@@ -16,7 +16,7 @@ async def msg(event):
     try:
         a = country.info()
     except:
-        await event.reply("Country Not Available Currently")
+        await event.reply("❍ Country Not Available Currently")
     name = a.get("name")
     bb = a.get("altSpellings")
     hu = ""
@@ -78,25 +78,25 @@ async def msg(event):
 
     caption = f"""<b><u>ɪɴғᴏʀᴍᴀᴛɪᴏɴ ɢᴀᴛʜᴇʀᴇᴅ sᴜᴄᴇssғᴜʟʟʏ </b></u>
 
-<b>ᴄᴏᴜɴᴛʀʏ ɴᴀᴍᴇ :</b> {name}
-<b>ᴀʟᴛᴇʀɴᴀᴛɪᴠᴇ sᴘᴇʟʟɪɴɢs :</b> {hu}
-<b>ᴄᴏᴜɴᴛʀʏ ᴀʀᴇᴀ :</b> {area} square kilometers
-<b>ʙᴏʀᴅᴇʀs :</b> {borders}
-<b>ᴄᴀʟʟɪɴɢ ᴄᴏᴅᴇs  :</b> {call}
-<b>ᴄᴏᴜɴᴛʀʏ's ᴄᴀᴘɪᴛᴀʟ :</b> {capital}
-<b>ᴄᴏᴜɴᴛʀʏ's ᴄᴜʀʀᴇɴᴄʏ :</b> {currencies}
-<b>ᴄᴏᴜɴᴛʀʏ's ғʟᴀɢ :</b> {okie}
-<b>ᴅᴇᴍᴏʏᴍ:</b> {HmM}
-<b>ᴄᴏᴜɴᴛʀʏ ᴛʏᴘᴇ :</b> {EsCoBaR}
-<b>ɪsᴏ ɴᴀᴍᴇs :</b> {iso}
-<b>ʟᴀɴɢᴜᴀɢᴇs :</b> {lMAO}
-<b>ɴᴀᴛɪᴠᴇ ɴᴀᴍᴇs :</b> {nonive}
-<b>ᴘᴏᴘᴜʟᴀᴛɪᴏɴs :</b> {waste}
-<b>ʀᴇɢɪᴏɴ :</b> {reg}
-<b>sᴜʙ ʀᴇɢɪᴏɴ :</b> {sub}
-<b>ᴛɪᴍᴇ ᴢᴏɴᴇs :</b> {tom}
-<b>ᴛᴏᴛᴀʟ ʟᴇᴠᴇʟ ᴅᴏᴍᴀɪɴ :</b> {lanester}
-<b>ᴡɪᴋɪᴘᴇᴅɪᴀ:</b> {wiki}
+<b>❍ ᴄᴏᴜɴᴛʀʏ ɴᴀᴍᴇ ➛</b> {name}
+<b>❍ ᴀʟᴛᴇʀɴᴀᴛɪᴠᴇ sᴘᴇʟʟɪɴɢs ➛</b> {hu}
+<b>❍ ᴄᴏᴜɴᴛʀʏ ᴀʀᴇᴀ ➛</b> {area} square kilometers
+<b>❍ ʙᴏʀᴅᴇʀs ➛</b> {borders}
+<b>❍ ᴄᴀʟʟɪɴɢ ᴄᴏᴅᴇs ➛</b> {call}
+<b>❍ ᴄᴏᴜɴᴛʀʏ's ᴄᴀᴘɪᴛᴀʟ ➛</b> {capital}
+<b>❍ ᴄᴏᴜɴᴛʀʏ's ᴄᴜʀʀᴇɴᴄʏ ➛</b> {currencies}
+<b>❍ ᴄᴏᴜɴᴛʀʏ's ғʟᴀɢ ➛</b> {okie}
+<b>❍ ᴅᴇᴍᴏʏᴍ ➛</b> {HmM}
+<b>❍ ᴄᴏᴜɴᴛʀʏ ᴛʏᴘᴇ ➛</b> {EsCoBaR}
+<b>❍ ɪsᴏ ɴᴀᴍᴇs ➛</b> {iso}
+<b>❍ ʟᴀɴɢᴜᴀɢᴇs ➛</b> {lMAO}
+<b>❍ ɴᴀᴛɪᴠᴇ ɴᴀᴍᴇs ➛</b> {nonive}
+<b>❍ ᴘᴏᴘᴜʟᴀᴛɪᴏɴs ➛</b> {waste}
+<b>❍ ʀᴇɢɪᴏɴ ➛</b> {reg}
+<b>❍ sᴜʙ ʀᴇɢɪᴏɴ ➛</b> {sub}
+<b>❍ ᴛɪᴍᴇ ᴢᴏɴᴇs ➛</b> {tom}
+<b>❍ ᴛᴏᴛᴀʟ ʟᴇᴠᴇʟ ᴅᴏᴍᴀɪɴ ➛</b> {lanester}
+<b>❍ ᴡɪᴋɪᴘᴇᴅɪᴀ ➛</b> {wiki}
 
 <u>ɪɴғᴏʀᴍᴀᴛɪᴏɴ ɢᴀᴛʜᴇʀᴇᴅ ʙʏ @{BOT_USERNAME}</u>
 """
