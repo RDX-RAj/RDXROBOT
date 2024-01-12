49d052b66c8f7f064ed1bf170bbb16d96db2e399
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-12 14:55:52 +0530

    Update chatgpt.py

--- MukeshRobot/modules/chatgpt.py
+++ MukeshRobot/modules/chatgpt.py
@@ -11,21 +11,21 @@ async def chat(bot, message):
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
         if len(message.command) < 2:
             await message.reply_text(
-            "Example:**\n\n`/chatgpt Where is TajMahal?`")
+            "❍ Example ➛ /gpt ᴡʜᴇʀᴇ ɪs ᴛᴀᴊᴍᴀʜᴀʟ ?")
         else:
             a = message.text.split(' ', 1)[1]
             response = requests.get(f'https://mukesh-api.vercel.app/chatgpt/{a}') 
             x=response.json()["results"]
             end_time = time.time()
             telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ᴍs"
-            await message.reply_text(f" {x}\n\n๏ᴛɪᴍᴇ ᴛᴀᴋᴇɴ ➛ {telegram_ping} \n\n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➛ ๛ᴀ ᴠ ɪ s ʜ ᴀ ༗ ", parse_mode=ParseMode.MARKDOWN)     
+            await message.reply_text(f" {x}\n\n❍ᴛɪᴍᴇ ᴛᴀᴋᴇɴ ➛ {telegram_ping} \n\n❍ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➛ ๛ᴀ ᴠ ɪ s ʜ ᴀ ༗ ", parse_mode=ParseMode.MARKDOWN)     
     except Exception as e:
         await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")
 
 __mod_name__ = "ᴀɪ-ɢᴘᴛ"
 __help__ = """
- Cʜᴀᴛɢᴘᴛ ᴄᴀɴ ᴀɴsᴡᴇʀ ʏᴏᴜʀ ǫᴜᴇsᴛɪᴏɴ  ᴀɴᴅ sʜᴏᴡs ʏᴏᴜ ᴛʜᴇ ʀᴇsᴜʟᴛ
+ ❍ ɢᴘᴛ ᴄᴀɴ ᴀɴsᴡᴇʀ ʏᴏᴜʀ ǫᴜᴇsᴛɪᴏɴ  ᴀɴᴅ sʜᴏᴡs ʏᴏᴜ ᴛʜᴇ ʀᴇsᴜʟᴛ
 
- ❍ /chatgpt  *:* ʀᴇᴘʟʏ ᴛo ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ
+ ❍ /gpt *➛* ʀᴇᴘʟʏ ᴛo ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ
  
  """
