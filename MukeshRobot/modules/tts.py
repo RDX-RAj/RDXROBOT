6c9b37499cad9e94e780263c39982ca00da74299
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-13 12:16:28 +0530

    Create tts.py

---
+++ MukeshRobot/modules/tts.py
@@ -0,0 +1,82 @@
+"""
+STATUS: Code is working. ✅
+"""
+
+"""
+GNU General Public License v3.0
+
+Copyright (C) 2022, SOME-1HING [https://github.com/SOME-1HING]
+
+Credits:-
+    I don't know who originally wrote this code. If you originally wrote this code, please reach out to me. 
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import os
+
+from gtts import gTTS
+from gtts import gTTSError
+from telethon import *
+from telethon.tl.types import *
+
+from MukeshRobot import *
+
+from MukeshRobot import telethn as tbot
+from MukeshRobot.events import register
+
+
+@register(pattern="^/tts (.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        text = previous_message.message
+        lan = input_str
+    elif "|" in input_str:
+        lan, text = input_str.split("|")
+    else:
+        await event.reply(
+            "Invalid Syntax\nFormat `/tts lang | text`\nFor eg: `/tts en | hello`"
+        )
+        return
+    text = text.strip()
+    lan = lan.strip()
+    try:
+        tts = gTTS(text, tld="com", lang=lan)
+        tts.save("k.mp3")
+    except AssertionError:
+        await event.reply(
+            "The text is empty.\n"
+            "Nothing left to speak after pre-precessing, "
+            "tokenizing and cleaning."
+        )
+        return
+    except ValueError:
+        await event.reply("Language is not supported.")
+        return
+    except RuntimeError:
+        await event.reply("Error loading the languages dictionary.")
+        return
+    except gTTSError:
+        await event.reply("Error in Google Text-to-Speech API request!")
+        return
+    with open("k.mp3", "r"):
+        await tbot.send_file(
+            event.chat_id, "k.mp3", voice_note=True, reply_to=reply_to_id
+        )
+        os.remove("k.mp3")
