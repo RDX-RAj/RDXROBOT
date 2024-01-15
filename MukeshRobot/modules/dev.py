d03a4a301712b56ef3b390b5393bb2d82a0ad86e
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-15 13:53:38 +0530

    Update dev.py

--- MukeshRobot/modules/dev.py
+++ MukeshRobot/modules/dev.py
@@ -17,16 +17,16 @@ from MukeshRobot.modules.helper_funcs.chat_status import dev_plus
 def allow_groups(update: Update, context: CallbackContext):
     args = context.args
     if not args:
-        update.effective_message.reply_text(f"Current state: {MukeshRobot.ALLOW_CHATS}")
+        update.effective_message.reply_text(f"✦ ᴄᴜʀʀᴇɴᴛ sᴛᴀᴛᴇ ᴏғ ᴀᴠɪsʜᴀ ✦ {MukeshRobot.ALLOW_CHATS}")
         return
     if args[0].lower() in ["off", "no"]:
         MukeshRobot.ALLOW_CHATS = True
     elif args[0].lower() in ["yes", "on"]:
         MukeshRobot.ALLOW_CHATS = False
     else:
-        update.effective_message.reply_text("Format: /lockdown Yes/No or Off/On")
+        update.effective_message.reply_text("✦ ғᴏʀᴍᴀᴛ ➛ /lockdown ʏᴇs/ɴᴏ ᴏʀ ᴏғғ/ᴏɴ")
         return
-    update.effective_message.reply_text("Done! Lockdown value toggled.")
+    update.effective_message.reply_text("✦ ᴅᴏɴᴇ ! ʟᴏᴄᴋᴅᴏᴡɴ ᴠᴀʟᴜᴇ ᴛᴏɢɢʟᴇᴅ.")
 
 
 @dev_plus
@@ -39,29 +39,29 @@ def leave(update: Update, context: CallbackContext):
             bot.leave_chat(int(chat_id))
         except TelegramError:
             update.effective_message.reply_text(
-                "Beep boop, I could not leave that group(dunno why tho)."
+                "✦ ʙᴇᴇᴘ ʙᴏᴏᴘ, ɪ ᴄᴏᴜʟᴅ ɴᴏᴛ ʟᴇᴀᴠᴇ ᴛʜᴀᴛ ɢʀᴏᴜᴘ(ᴅᴜɴɴᴏ ᴡʜʏ ᴛʜᴏ)."
             )
             return
         with suppress(Unauthorized):
-            update.effective_message.reply_text("Beep boop, I left that soup!.")
+            update.effective_message.reply_text("✦ ʙᴇᴇᴘ ʙᴏᴏᴘ, ɪ ʟᴇғᴛ ᴛʜᴀᴛ sᴏᴜᴘ !.")
     else:
-        update.effective_message.reply_text("Send a valid chat ID")
+        update.effective_message.reply_text("✦ sᴇɴᴅ ᴀ ᴠᴀʟɪᴅ ᴄʜᴀᴛ ɪᴅ")
 
 
 @dev_plus
 def gitpull(update: Update, context: CallbackContext):
     sent_msg = update.effective_message.reply_text(
-        "Pulling all changes from remote and then attempting to restart."
+        "✦ ᴘᴜʟʟɪɴɢ ᴀʟʟ ᴄʜᴀɴɢᴇs ғʀᴏᴍ ʀᴇᴍᴏᴛᴇ ᴀɴᴅ ᴛʜᴇɴ ᴀᴛᴛᴇᴍᴘᴛɪɴɢ ᴛᴏ ʀᴇsᴛᴀʀᴛ."
     )
     subprocess.Popen("git pull", stdout=subprocess.PIPE, shell=True)
 
-    sent_msg_text = sent_msg.text + "\n\nChanges pulled...I guess.. Restarting in "
+    sent_msg_text = sent_msg.text + "\n\n❍ ᴄʜᴀɴɢᴇs ᴘᴜʟʟᴇᴅ...ɪ ɢᴜᴇss.. ʀᴇsᴛᴀʀᴛɪɴɢ ɪɴ "
 
     for i in reversed(range(5)):
         sent_msg.edit_text(sent_msg_text + str(i + 1))
         sleep(1)
 
-    sent_msg.edit_text("Restarted.")
+    sent_msg.edit_text("✦ ʀᴇsᴛᴀʀᴛᴇᴅ.")
 
     os.system("restart.bat")
     os.execv("start.bat", sys.argv)
@@ -70,7 +70,7 @@ def gitpull(update: Update, context: CallbackContext):
 @dev_plus
 def restart(update: Update, context: CallbackContext):
     update.effective_message.reply_text(
-        "Starting a new instance and shutting down this one"
+        "✦ sᴛᴀʀᴛɪɴɢ ᴀ ɴᴇᴡ ɪɴsᴛᴀɴᴄᴇ ᴀɴᴅ sʜᴜᴛᴛɪɴɢ ᴅᴏᴡɴ ᴛʜɪs ᴏɴᴇ"
     )
 
     os.system("restart.bat")
@@ -87,6 +87,11 @@ dispatcher.add_handler(LEAVE_HANDLER)
 dispatcher.add_handler(GITPULL_HANDLER)
 dispatcher.add_handler(RESTART_HANDLER)
 
-
+__mod_name__ = "ᴏᴡɴᴇʀ"
+__help__ = """
+❍ /leave ➛ ʟᴇᴀᴠᴇ ᴛʜᴇ ᴄʜᴀᴛ.
+❍ /gitpull ➛ ɢɪᴛғᴜʟʟ ᴛʜᴇ ʙᴏᴛ.
+❍ /reboot ➛ ʀᴇʙᴏᴏᴛ ᴛʜᴇ ʙᴏᴛ.
+❍ /lockdown ➛ ʟᴏᴄᴋᴅᴏᴡɴ ᴛᴏ ᴀʟʟ ɢʀᴏᴜᴘs """
 
 __handlers__ = [LEAVE_HANDLER, GITPULL_HANDLER, RESTART_HANDLER, ALLOWGROUPS_HANDLER]
