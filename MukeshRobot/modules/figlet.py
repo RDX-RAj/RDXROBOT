cee91c9c38cdd6fcd18c78ddd523d6fd0cecdc86
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-15 14:10:33 +0530

    Update figlet.py

--- MukeshRobot/modules/figlet.py
+++ MukeshRobot/modules/figlet.py
@@ -18,18 +18,18 @@ async def echo(bot, message):
     try:
         text = message.text.split(' ',1)[1]
     except IndexError:
-        return await message.reply_text("Example:\n\n`/figlet Mukesh`")
+        return await message.reply_text("❍ ᴇxᴀᴍᴘʟᴇ ➛ /figlet ʀᴏʏ-ᴇᴅɪᴛx")
     kul_text, keyboard = figle(text)
-    await message.reply_text(f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ ғɪɢʟᴇᴛ :\n<pre>{kul_text}</pre>", quote=True, reply_markup=keyboard)
+    await message.reply_text(f"❍ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ғɪɢʟᴇᴛ ➛\n<pre>{kul_text}</pre>", quote=True, reply_markup=keyboard)
 
 @Client.on_callback_query(filters.regex("figlet"))
 async def figlet_handler(Client, query: CallbackQuery):
   try:
       kul_text, keyboard = figle(text)
-      await query.message.edit_text(f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ ғɪɢʟᴇᴛ :\n<pre>{kul_text}</pre>", reply_markup=keyboard)
+      await query.message.edit_text(f"❍ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ғɪɢʟᴇᴛ ➛\n<pre>{kul_text}</pre>", reply_markup=keyboard)
   except Exception as e : 
       await message.reply(e)
-__mod_name__ = "Fɪɢʟᴇᴛ" 
+__mod_name__ = "ғɪɢʟᴇᴛ" 
 __help__="""
-❍ /figlet*:* ᴍᴀᴋᴇs ғɪɢʟᴇᴛ ᴏғ ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ
-Example:\n\n`/figlet Mukesh`"""
+❍ /figlet* ➛* ᴍᴀᴋᴇs ғɪɢʟᴇᴛ ᴏғ ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ
+❍ ᴇxᴀᴍᴘʟᴇ ➛ /figlet ʀᴏʏ-ᴇᴅɪᴛx"""
