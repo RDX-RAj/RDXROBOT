dc202d0c7001ccca9f0ffda86f7fd6793782dcfc
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-15 14:02:42 +0530

    Update fakeinfo.py

--- MukeshRobot/modules/fakeinfo.py
+++ MukeshRobot/modules/fakeinfo.py
@@ -44,7 +44,7 @@ from MukeshRobot import pbot
 @tbot.on(events.NewMessage(pattern="/fakegen$"))
 async def hi(event):
     fake = Faker()
-    print("FAKE DETAILS GENERATED\n")
+    print("❍ ғᴀᴋᴇ ᴅᴇᴛᴀɪʟs ɢᴇɴᴇʀᴀᴛᴇᴅ\n")
     name = str(fake.name())
     fake.add_provider(internet)
     address = str(fake.address())
@@ -55,23 +55,22 @@ async def hi(event):
     android = fake.android_platform_token()
     pc = fake.chrome()
     await event.reply(
-        f"<b><u> Fake Information Generated</b></u>\n<b>Name :-</b><code>{name}</code>\n\n<b>Address:-</b><code>{address}</code>\n\n<b>IP ADDRESS:-</b><code>{ip}</code>\n\n<b>credit card:-</b><code>{cc}</code>\n\n<b>Email Id:-</b><code>{email}</code>\n\n<b>Job:-</b><code>{job}</code>\n\n<b>android user agent:-</b><code>{android}</code>\n\n<b>Pc user agent:-</b><code>{pc}</code>",
+        f"<b>✦ ғᴀᴋᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b> ✦\n<b>❍ ɴᴀᴍᴇ ➛ </b><code>{name}</code>\n\n<b>❍ ᴀᴅᴅʀᴇss ➛ </b><code>{address}</code>\n\n<b>❍ ɪᴘ ᴀᴅᴅʀᴇss ➛ </b><code>{ip}</code>\n\n<b>❍ ᴄʀᴇᴅɪᴛ ᴄᴀʀᴅ ➛ </b><code>{cc}</code>\n\n<b>❍ ᴇᴍᴀɪʟ ɪᴅ ➛ </b><code>{email}</code>\n\n<b>❍ ᴊᴏʙ ➛ </b><code>{job}</code>\n\n<b>❍ ᴀɴᴅʀᴏɪᴅ ᴜsᴇʀ ᴀɢᴇɴᴛ ➛ </b><code>{android}</code>\n\n<b>❍ ᴘᴄ ᴜsᴇʀ ᴀɢᴇɴᴛ ➛ </b><code>{pc}</code>",
         parse_mode="HTML",
     )
 
 @pbot.on_message(filters.command('picgen'))
 async def picgen(_, message):
     img = "https://thispersondoesnotexist.com/image"
-    text = f"Fake Image successfully generated."
+    text = f"❍ ғᴀᴋᴇ ɪᴍᴀɢᴇ sᴜᴄᴄᴇssғᴜʟʟʏ ɢᴇɴᴇʀᴀᴛᴇᴅ."
     await message.reply_photo(photo=img, caption=text)
 
 
 
 
-__mod_name__ = "Fake info"
+__mod_name__ = "ғᴀᴋᴇ-ɪɴғᴏ"
 
 __help__ = """
-*Commands:*
-- `/fakegen` : Generates Fake Information
-- `/picgen` : Generate a Fake pic
+❍ `/fakegen` ➛ ɢᴇɴᴇʀᴀᴛᴇs ғᴀᴋᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ
+❍ `/picgen` ➛ ɢᴇɴᴇʀᴀᴛᴇ ᴀ ғᴀᴋᴇ ᴘɪᴄ
 """
