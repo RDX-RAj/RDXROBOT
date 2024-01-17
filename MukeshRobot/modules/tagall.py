fd03d8ef8351535ffd793db3ef8e0b1871609ef8
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-17 14:29:38 +0530

    Update tagall.py

--- MukeshRobot/modules/tagall.py
+++ MukeshRobot/modules/tagall.py
@@ -57,10 +57,10 @@ async def mentionall(event):
         if not chat_id in spam_chats:
             break
         usrnum += 1
-        usrtxt += f"✦ [{usr.first_name}](tg://user?id={usr.id})\n"
-        if usrnum == 5:
+        usrtxt += f"✦ [{usr.first_name}](tg://user?id={usr.id})\n\n"
+        if usrnum == 3:
             if mode == "text_on_cmd":
-                txt = f"{msg}\n{usrtxt}"
+                txt = f"{msg}\n\n{usrtxt}"
                 await client.send_message(chat_id, txt)
             elif mode == "text_on_reply":
                 await msg.reply(usrtxt)
