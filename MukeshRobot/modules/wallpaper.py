86e51e709ca8fb8b8d5f76a5dd482e506c6a324d
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-18 13:38:58 +0530

    Update wallpaper.py

--- MukeshRobot/modules/wallpaper.py
+++ MukeshRobot/modules/wallpaper.py
@@ -8,20 +8,20 @@ from MukeshRobot import pbot
 
 @pbot.on_message(filters.command(["wall", "wallpaper"]))
 async def wall(_, message: Message):
-    " ғɪxᴇᴅ ᴡᴀʟʟ ʙʏ ᴍᴜᴋᴇsʜ"
+    "❍ ғɪxᴇᴅ ᴡᴀʟʟ ʙʏ ʀᴏʏ-ᴇᴅɪᴛx"
     try:
         text = message.text.split(None, 1)[1]
     except IndexError:
         text = None
     if not text:
-        return await message.reply_text("`Please give some query to search.`")
-    m = await message.reply_text("`Searching for wallpapers...`")
+        return await message.reply_text("❍ `ᴘʟᴇᴀsᴇ ɢɪᴠᴇ sᴏᴍᴇ ǫᴜᴇʀʏ ᴛᴏ sᴇᴀʀᴄʜ.`")
+    m = await message.reply_text("❍ `sᴇᴀʀᴄʜɪɴɢ ғᴏʀ ᴡᴀʟʟᴘᴀᴘᴇʀs...`")
     try:
         url = requests.get(f"https://api.safone.me/wall?query={text}").json()["results"]
         ran = random.randint(0, 3)
         await message.reply_photo(
             photo=url[ran]["imageUrl"],
-            caption=f"🥀 **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}",
+            caption=f"❍ **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ ➛** {message.from_user.mention}",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [InlineKeyboardButton("ʟɪɴᴋ", url=url[ran]["imageUrl"])],
@@ -31,5 +31,10 @@ async def wall(_, message: Message):
         await m.delete()
     except Exception as e:
         await m.edit_text(
-            f"`ᴡᴀʟʟᴘᴀᴘᴇʀ ɴᴏᴛ ғᴏᴜɴᴅ ғᴏʀ : `{text}`",
+            f"❍ `ᴡᴀʟʟᴘᴀᴘᴇʀ ɴᴏᴛ ғᴏᴜɴᴅ ғᴏʀ ➛ `{text}`",
         )
+
+__mod_name__ = "ᴡᴀʟʟ"
+__help__ = """
+ ❍ /ᴡᴀʟʟᴘᴀᴘᴇʀ ➛ ʀᴀɴᴅᴏᴍ ᴡᴀʟʟᴘᴀᴘᴇʀ ɪᴍᴀɢᴇs.
+ """
