6c2c3f0161d99133eb9fae9061ce638b0e232ca1
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-20 05:52:54 +0530

    Update and rename good_night.py# to good_night.py

--- MukeshRobot/modules/good_night.py
+++ MukeshRobot/modules/good_night.py
@@ -25,7 +25,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from telethon import Button
 
-from MukeshRobot import telethon as tbot
+from MukeshRobot import telethon as telethn
 from MukeshRobot.events import register
 
 PHOTO = "https://te.legra.ph/file/4e959d8f074bef7061463.mp4"
