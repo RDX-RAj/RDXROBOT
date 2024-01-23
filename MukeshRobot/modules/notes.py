82687ed0c5cc339f35630bd836eb25dc64abb717
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-01-23 08:01:36 +0530

    Update notes.py

--- MukeshRobot/modules/notes.py
+++ MukeshRobot/modules/notes.py
@@ -477,7 +477,7 @@ def __import_data__(chat_id, data):
 
 
 def __stats__():
-    return f"❅ ᴛᴏᴛᴇʟ ɴᴏᴛᴇs ➛ {sql.num_notes()}\n✦ ᴀᴄʀᴏss ᴄʜᴀᴛs ➛ {sql.num_chats()}\n\n"
+    return f"❅ ᴛᴏᴛᴇʟ ɴᴏᴛᴇs ➛ {sql.num_notes()}\n✦ ᴀᴄʀᴏss ᴄʜᴀᴛs ➛ {sql.num_chats()}\n"
 
 
 def __migrate__(old_chat_id, new_chat_id):
