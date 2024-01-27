0d0cbb1ddbe4a34727a39cfbe5b7cbf8eab5f916
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-01-27 22:06:09 +0530

    Update muting.py

--- MukeshRobot/modules/muting.py
+++ MukeshRobot/modules/muting.py
@@ -294,5 +294,5 @@ dispatcher.add_handler(MUTE_HANDLER)
 dispatcher.add_handler(UNMUTE_HANDLER)
 dispatcher.add_handler(TEMPMUTE_HANDLER)
 
-__mod_name__ = "Mᴜᴛᴇ​"
+__mod_name__ = "ᴍᴜᴛᴇ​"
 __handlers__ = [DMUTE_HANDLER,MUTE_HANDLER, UNMUTE_HANDLER, TEMPMUTE_HANDLER]
